7bc304a3b9419bf3d7b7f619d0704f2d55a2eb36
Gustav Brodin <brodin.gustav@gmail.com>
2025-02-18 11:01:22 +0100

    Commit - Justerat footer

--- obj/Debug/net9.0/dt191g_mom3.AssemblyInfo.cs
+++ obj/Debug/net9.0/dt191g_mom3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dt191g_mom3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b8c45fa244ff296583c52d80c53841aa371eebd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e7d4ff9d23b68032e08f72d279f3f957098cfff")]
 [assembly: System.Reflection.AssemblyProductAttribute("dt191g_mom3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dt191g_mom3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
