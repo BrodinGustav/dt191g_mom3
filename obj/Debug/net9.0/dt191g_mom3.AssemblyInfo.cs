2674334b352f79c2f63a7c6a8071bd28f14462db
Gustav Brodin <brodin.gustav@gmail.com>
2025-02-18 09:45:51 +0100

    Commit

--- obj/Debug/net9.0/dt191g_mom3.AssemblyInfo.cs
+++ obj/Debug/net9.0/dt191g_mom3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dt191g_mom3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ac7462e2d3102cf09f5982725927a96dba449e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4571fb2fb18818dfa0b8c4c82a86db45d20810b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("dt191g_mom3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dt191g_mom3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
